79432187c12565f8ba49dbb1141d6e44c48b3703
Péter Maróti <maroti.peter@gmail.com>
2014-12-24 19:15:55 +0100

    add sass to project and main application scss file

---
+++ config.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+sass_dir = "sass"
+css_dir = "public/css"
+images_dir = "public/img"
+javascripts_dir = "public/js"
+
+output_style = :compressed
+
+line_comments = false
+
+preferred_syntax = :scss
