3ef1e3f8829692bc54e88308b7fc927f003dcf9c
Péter Maróti <maroti.peter@gmail.com>
2014-12-24 19:13:28 +0100

    change users table schema
    need all the columns to be able to use the default auth methods such as
    loging and logout

--- app/database/migrations/2014_12_14_132225_create_users_table.php
+++ app/database/migrations/2014_12_14_132225_create_users_table.php
@@ -13,9 +13,11 @@ class CreateUsersTable extends Migration {
 	public function up()
 	{
 		Schema::create('users', function(Blueprint $table){
+			$table->increments('id');
 			$table->string('email', 255);
 			$table->string('password', 70);
-			$table->primary('email');
+			$table->timestamps();
+			$table->rememberToken();
 		});
 	}
 
