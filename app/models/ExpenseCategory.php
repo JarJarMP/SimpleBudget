1ae78bdab1e4dec4d2f680808da9ada58476b093
Péter Maróti <maroti.peter@gmail.com>
2014-12-26 14:24:41 +0100

    add - category models based on baum nested

---
+++ app/models/ExpenseCategory.php
@@ -0,0 +1,8 @@
+<?php
+
+use Baum\Node;
+
+class ExpenseCategory extends Node
+{
+	protected $table = 'expense_categories';
+}
---
+++ app/models/IncomeCategory.php
@@ -0,0 +1,8 @@
+<?php
+
+use Baum\Node;
+
+class IncomeCategory extends Node
+{
+	protected $table = 'income_categories';
+}
