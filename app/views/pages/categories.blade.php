161be650928d736dde2c7be29fde23f3035e8ca7
Péter Maróti <maroti.peter@gmail.com>
2015-01-11 20:06:48 +0100

    change - create view file for progressbar

--- app/views/pages/categories.blade.php
+++ app/views/pages/categories.blade.php
@@ -12,18 +12,7 @@
 
 				<div class="panel-body">
 					<div id="expense_tree">
-						<div class="progress">
-							<div 
-								class="progress-bar progress-bar-striped active" 
-								role="progressbar" 
-								aria-valuenow="100" 
-								aria-valuemin="0" 
-								aria-valuemax="100" 
-								style="width: 100%"
-							>
-								<span class="sr-only">Categories are loading</span>
-							</div>
-						</div>
+						{{ View::make('partials.progressbar', array('text' => 'Categories are loading')) }}
 					</div>
 				</div>
 			</div>
@@ -37,18 +26,7 @@
 
 				<div class="panel-body">
 					<div id="income_tree">
-						<div class="progress">
-							<div 
-								class="progress-bar progress-bar-striped active" 
-								role="progressbar" 
-								aria-valuenow="100" 
-								aria-valuemin="0" 
-								aria-valuemax="100" 
-								style="width: 100%"
-							>
-								<span class="sr-only">Categories are loading</span>
-							</div>
-						</div>
+						{{ View::make('partials.progressbar', array('text' => 'Categories are loading')) }}
 					</div>
 				</div>
 			</div>
